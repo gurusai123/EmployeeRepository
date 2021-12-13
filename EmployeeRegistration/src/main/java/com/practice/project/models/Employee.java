e48edfbf38094d004a6c8cb14c0572f17127bb86
Sarma <916982@cognizant.com>
2021-12-13 15:24:16 +0530

    v2.0

--- EmployeeRegistration/src/main/java/com/practice/project/models/Employee.java
+++ EmployeeRegistration/src/main/java/com/practice/project/models/Employee.java
@@ -1,5 +1,7 @@
 package com.practice.project.models;
 
+import java.util.Date;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -20,27 +22,30 @@ import lombok.Data;
 @Data
 @Document
 public class Employee {
-	@Id
-	@NotNull(message = "")
-	@Pattern(regexp = "[0-9]{3,5}")
+//	@Id
+//	@NotNull(message = "")
+//	@Pattern(regexp = "[0-9]{3,5}")
 	private String id;
-	@NotNull(message = "")
-	@Pattern(regexp = "[A-Za-z]")
-	@Size(min = 3, max = 20)
+//	@NotNull(message = "")
+//	@Pattern(regexp = "[A-Za-z]")
+//	@Size(min = 3, max = 20)
 	private String firstName;
-	@NotNull(message = "")
-	@Pattern(regexp = "[A-Za-z]")
-	@Size(min = 3, max = 20)
+//	@NotNull(message = "")
+//	@Pattern(regexp = "[A-Za-z]")
+//	@Size(min = 3, max = 20)
 	private String lastName;
 	@DBRef
 	@Autowired
 	private Address address;
-	@NotNull(message = "")
-	@Pattern(regexp = "[A-Za-z0-9@_]")
+//	@NotNull(message = "")
+//	@Pattern(regexp = "[A-Za-z0-9@_]")
 	@UniqueElements
 	private String userName;
-	@NotNull(message = "")
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
-	@JsonProperty(access = Access.WRITE_ONLY)
+//	@NotNull(message = "")
+//	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+//	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
+	private String company;
+	private String designation;
+	private Date DOB; 
 }
